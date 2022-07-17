e707a10ec9a649a7fc038c0f8b9ff3dda0ffbd31
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-07-17 09:21:28 -0700

    Added exe and source files for Simple Calc

---
+++ Simple Sales Calc/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿
+namespace CSC670_Project2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.productOneInfoLabel = new System.Windows.Forms.Label();
+            this.product2InfoLabel = new System.Windows.Forms.Label();
+            this.product3InfoLabel = new System.Windows.Forms.Label();
+            this.product1QuanLabel = new System.Windows.Forms.Label();
+            this.productOneTextBox = new System.Windows.Forms.TextBox();
+            this.productTwoTextBox = new System.Windows.Forms.TextBox();
+            this.productThreeTextBox = new System.Windows.Forms.TextBox();
+            this.product2QuanLabel = new System.Windows.Forms.Label();
+            this.product3QuanLabel = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.prod1SaleInfo = new System.Windows.Forms.Label();
+            this.prod2SaleInfo = new System.Windows.Forms.Label();
+            this.prod3SaleInfo = new System.Windows.Forms.Label();
+            this.highestQuanSaleInfo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(12, 45);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(800, 68);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Sales App";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.product3InfoLabel);
+            this.groupBox1.Controls.Add(this.product2InfoLabel);
+            this.groupBox1.Controls.Add(this.productOneInfoLabel);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(83, 186);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 246);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Product Info";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.product3QuanLabel);
+            this.groupBox2.Controls.Add(this.product2QuanLabel);
+            this.groupBox2.Controls.Add(this.productThreeTextBox);
+            this.groupBox2.Controls.Add(this.productTwoTextBox);
+            this.groupBox2.Controls.Add(this.productOneTextBox);
+            this.groupBox2.Controls.Add(this.product1QuanLabel);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(439, 186);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(304, 246);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Quantity Sold";
+            // 
+            // productOneInfoLabel
+            // 
+            this.productOneInfoLabel.AutoSize = true;
+            this.productOneInfoLabel.Location = new System.Drawing.Point(28, 57);
+            this.productOneInfoLabel.Name = "productOneInfoLabel";
+            this.productOneInfoLabel.Size = new System.Drawing.Size(213, 31);
+            this.productOneInfoLabel.TabIndex = 0;
+            this.productOneInfoLabel.Text = "Product 1: $2.98";
+            // 
+            // product2InfoLabel
+            // 
+            this.product2InfoLabel.AutoSize = true;
+            this.product2InfoLabel.Location = new System.Drawing.Point(28, 119);
+            this.product2InfoLabel.Name = "product2InfoLabel";
+            this.product2InfoLabel.Size = new System.Drawing.Size(213, 31);
+            this.product2InfoLabel.TabIndex = 1;
+            this.product2InfoLabel.Text = "Product 2: $4.50";
+            // 
+            // product3InfoLabel
+            // 
+            this.product3InfoLabel.AutoSize = true;
+            this.product3InfoLabel.Location = new System.Drawing.Point(28, 181);
+            this.product3InfoLabel.Name = "product3InfoLabel";
+            this.product3InfoLabel.Size = new System.Drawing.Size(213, 31);
+            this.product3InfoLabel.TabIndex = 2;
+            this.product3InfoLabel.Text = "Product 3: $9.98";
+            // 
+            // product1QuanLabel
+            // 
+            this.product1QuanLabel.AutoSize = true;
+            this.product1QuanLabel.Location = new System.Drawing.Point(25, 57);
+            this.product1QuanLabel.Name = "product1QuanLabel";
+            this.product1QuanLabel.Size = new System.Drawing.Size(130, 31);
+            this.product1QuanLabel.TabIndex = 0;
+            this.product1QuanLabel.Text = "Product 1";
+            // 
+            // productOneTextBox
+            // 
+            this.productOneTextBox.Location = new System.Drawing.Point(178, 54);
+            this.productOneTextBox.Name = "productOneTextBox";
+            this.productOneTextBox.Size = new System.Drawing.Size(120, 38);
+            this.productOneTextBox.TabIndex = 1;
+            // 
+            // productTwoTextBox
+            // 
+            this.productTwoTextBox.Location = new System.Drawing.Point(178, 116);
+            this.productTwoTextBox.Name = "productTwoTextBox";
+            this.productTwoTextBox.Size = new System.Drawing.Size(120, 38);
+            this.productTwoTextBox.TabIndex = 2;
+            // 
+            // productThreeTextBox
+            // 
+            this.productThreeTextBox.Location = new System.Drawing.Point(178, 178);
+            this.productThreeTextBox.Name = "productThreeTextBox";
+            this.productThreeTextBox.Size = new System.Drawing.Size(120, 38);
+            this.productThreeTextBox.TabIndex = 3;
+            // 
+            // product2QuanLabel
+            // 
+            this.product2QuanLabel.AutoSize = true;
+            this.product2QuanLabel.Location = new System.Drawing.Point(25, 119);
+            this.product2QuanLabel.Name = "product2QuanLabel";
+            this.product2QuanLabel.Size = new System.Drawing.Size(130, 31);
+            this.product2QuanLabel.TabIndex = 4;
+            this.product2QuanLabel.Text = "Product 2";
+            // 
+            // product3QuanLabel
+            // 
+            this.product3QuanLabel.AutoSize = true;
+            this.product3QuanLabel.Location = new System.Drawing.Point(25, 181);
+            this.product3QuanLabel.Name = "product3QuanLabel";
+            this.product3QuanLabel.Size = new System.Drawing.Size(130, 31);
+            this.product3QuanLabel.TabIndex = 5;
+            this.product3QuanLabel.Text = "Product 3";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.highestQuanSaleInfo);
+            this.groupBox3.Controls.Add(this.prod3SaleInfo);
+            this.groupBox3.Controls.Add(this.prod2SaleInfo);
+            this.groupBox3.Controls.Add(this.prod1SaleInfo);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(83, 467);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(660, 299);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sales Information";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(593, 787);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(150, 57);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(425, 787);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(150, 57);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Product 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 31);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Product 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 31);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Product 3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "High quantity sold";
+            // 
+            // prod1SaleInfo
+            // 
+            this.prod1SaleInfo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.prod1SaleInfo.Location = new System.Drawing.Point(333, 50);
+            this.prod1SaleInfo.Name = "prod1SaleInfo";
+            this.prod1SaleInfo.Size = new System.Drawing.Size(273, 38);
+            this.prod1SaleInfo.TabIndex = 4;
+            // 
+            // prod2SaleInfo
+            // 
+            this.prod2SaleInfo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.prod2SaleInfo.Location = new System.Drawing.Point(333, 112);
+            this.prod2SaleInfo.Name = "prod2SaleInfo";
+            this.prod2SaleInfo.Size = new System.Drawing.Size(273, 38);
+            this.prod2SaleInfo.TabIndex = 5;
+            // 
+            // prod3SaleInfo
+            // 
+            this.prod3SaleInfo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.prod3SaleInfo.Location = new System.Drawing.Point(333, 174);
+            this.prod3SaleInfo.Name = "prod3SaleInfo";
+            this.prod3SaleInfo.Size = new System.Drawing.Size(273, 38);
+            this.prod3SaleInfo.TabIndex = 6;
+            // 
+            // highestQuanSaleInfo
+            // 
+            this.highestQuanSaleInfo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.highestQuanSaleInfo.Location = new System.Drawing.Point(333, 236);
+            this.highestQuanSaleInfo.Name = "highestQuanSaleInfo";
+            this.highestQuanSaleInfo.Size = new System.Drawing.Size(273, 38);
+            this.highestQuanSaleInfo.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(257, 787);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 57);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 879);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label product3InfoLabel;
+        private System.Windows.Forms.Label product2InfoLabel;
+        private System.Windows.Forms.Label productOneInfoLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label product3QuanLabel;
+        private System.Windows.Forms.Label product2QuanLabel;
+        private System.Windows.Forms.TextBox productThreeTextBox;
+        private System.Windows.Forms.TextBox productTwoTextBox;
+        private System.Windows.Forms.TextBox productOneTextBox;
+        private System.Windows.Forms.Label product1QuanLabel;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label highestQuanSaleInfo;
+        private System.Windows.Forms.Label prod3SaleInfo;
+        private System.Windows.Forms.Label prod2SaleInfo;
+        private System.Windows.Forms.Label prod1SaleInfo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Simple Sales Calc/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSC670_Project2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static double productOne, productTwo, productThree;
+        private static double p1Quan, p2Quan, p3Quan;
+        private static double p1Sales, p2Sales, p3Sales, highestSale;
+        private static int highestSwitch;
+
+        public static double Max(double x, double y, double z)
+        {
+            return Math.Max(x, Math.Max(y, z));
+        }
+
+        private void calc_Click(object sender, EventArgs e)
+        {
+            productOne = 2.98;
+            productTwo = 4.5;
+            productThree = 9.98;
+
+            try
+            {
+                if (productOneTextBox.Text != null)
+                {
+                    p1Quan = Convert.ToDouble(productOneTextBox.Text);
+                    p1Sales = productOne * p1Quan;
+                }
+                if (productTwoTextBox.Text != null)
+                {
+                    p2Quan = Convert.ToDouble(productTwoTextBox.Text);
+                    p2Sales = productTwo * p2Quan;
+                }
+                if (productOneTextBox.Text != null)
+                {
+                    p3Quan = Convert.ToDouble(productThreeTextBox.Text);
+                    p3Sales = productThree * p3Quan;
+                }
+                
+            }
+            catch (Exception) 
+            {
+                MessageBox.Show("Please enter numerical values", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                p1Sales = 0;
+                p2Sales = 0;
+                p3Sales = 0;
+            }
+
+            prod1SaleInfo.Text = p1Sales.ToString("c");
+            prod2SaleInfo.Text = p2Sales.ToString("c");
+            prod3SaleInfo.Text = p3Sales.ToString("c");
+
+            highestSale = Max(p1Sales, p2Sales, p3Sales);
+            if (highestSale == p1Sales)
+            {
+                highestSwitch = 1;
+            }
+            else if (highestSale == p2Sales)
+            {
+                highestSwitch = 2;
+            }
+            else
+            {
+                highestSwitch = 3;
+            }
+
+            switch (highestSwitch)
+            {
+                case 1:
+                    highestQuanSaleInfo.Text = String.Format("Product 1 with {0}", p1Quan);
+                    break;
+                case 2: 
+                    highestQuanSaleInfo.Text = String.Format("Product 2 with {0}", p2Quan);
+                    break;
+                case 3:
+                    highestQuanSaleInfo.Text = String.Format("Product 3 with {0}", p3Quan);
+                    break;
+                default:
+                    highestQuanSaleInfo.Text = "Default";
+                    break;
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            p1Quan = 0;
+            p2Quan = 0;
+            p3Quan = 0;
+
+            productOneTextBox.Clear();
+            productTwoTextBox.Clear();
+            productThreeTextBox.Clear();
+
+            prod1SaleInfo.Text = "";
+            prod2SaleInfo.Text = "";
+            prod3SaleInfo.Text = "";
+            highestQuanSaleInfo.Text = "";
+        }
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
